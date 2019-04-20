aada6f7129f77abefab193457d127de254e92302
cookcloud <49710111+cookcloud@users.noreply.github.com>
2019-04-20 15:55:30 +0800

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
         System.out.println("hello,github");
         System.out.println("使用git协议进行提交项目");
         System.out.println("这是第二次提交");
+        System.out.println("在github远程仓库上直接进行修改");
+
     }
 }
