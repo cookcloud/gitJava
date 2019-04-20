70288503cf0302ee4db34e2bcbd9bebfb87753bd
cookcloud <cookcloud@aliyun.com>
2019-04-20 21:33:56 +0800

    本地分支bb第二次更新

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
      System.out.println("这是bb分支第一次更新");
      System.out.println("这是远程bb分支第一次更新");
+     System.out.println("这是本地bb分支第二次更新");
     }
 }
