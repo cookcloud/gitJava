412bda9948399ce754118c1a7b11e8f79bbd5f90
cookcloud <cookcloud@aliyun.com>
2019-04-20 20:22:44 +0800

    情况hello.txt

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,9 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello,github");
-        System.out.println("使用git协议进行提交项目");
-        System.out.println("这是第二次提交");
-        System.out.println("在github远程仓库上直接进行修改");
-
+      
     }
 }
